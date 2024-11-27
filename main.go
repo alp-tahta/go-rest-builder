ca26d6873a41f4e9e24fa83f6cbf6c81ed6c7c95
Alp Tahta <alp.tht48@gmail.com>
2024-11-27 10:45:07 +0300

    Implement functionality for creating new project folder and go mod file

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+// DEFAULT_PROJECT_NAME is the name for newly created project, If user does not provide a project name value
+const DEFAULT_PROJECT_NAME = "web-api"
 
 func main() {
-	fmt.Println("Test")
+	// Create a directory for the new project
+	if err := os.Mkdir(DEFAULT_PROJECT_NAME, 0755); err != nil {
+		log.Fatalf("Error creating project directory: %v\n", err)
+	}
+
+	// Change to the new project directory
+	projectPath := filepath.Join(".", DEFAULT_PROJECT_NAME)
+	if err := os.Chdir(projectPath); err != nil {
+		log.Fatalf("Error changing to project directory: %v\n", err)
+	}
+
+	// Initialize a new go.mod file
+	cmd := exec.Command("go", "mod", "init", DEFAULT_PROJECT_NAME)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error running 'go mod init': %v\n", err)
+	}
+
+	log.Println("Go project created successfully!")
 }
