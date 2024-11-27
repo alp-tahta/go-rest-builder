930ad2607ea81f3a913eb4d8520e41259ea26cf5
Alp Tahta <alp.tht48@gmail.com>
2024-11-27 20:14:20 +0300

    Edit main

--- cmd/go-rest-builder/main.go
+++ cmd/go-rest-builder/main.go
@@ -1,35 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
-)
-
-// DEFAULT_PROJECT_NAME is the name for newly created project, If user does not provide a project name value
-const DEFAULT_PROJECT_NAME = "web-api"
+import "fmt"
 
 func main() {
-	// Create a directory for the new project
-	if err := os.Mkdir(DEFAULT_PROJECT_NAME, 0755); err != nil {
-		log.Fatalf("Error creating project directory: %v\n", err)
-	}
-
-	// Change to the new project directory
-	projectPath := filepath.Join(".", DEFAULT_PROJECT_NAME)
-	if err := os.Chdir(projectPath); err != nil {
-		log.Fatalf("Error changing to project directory: %v\n", err)
-	}
-
-	// Initialize a new go.mod file
-	cmd := exec.Command("go", "mod", "init", DEFAULT_PROJECT_NAME)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Error running 'go mod init': %v\n", err)
-	}
-
-	log.Println("Go project created successfully!")
+	fmt.Println("test")
 }
